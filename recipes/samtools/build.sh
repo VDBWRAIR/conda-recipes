b1791415a576dc49b0187e3206c7aef767ceb8fc
Tyghe Vallard <vallardt@gmail.com>
2016-03-24 03:57:05 -0400

    should now build with conda zlib and ncurses

--- recipes/samtools/build.sh
+++ recipes/samtools/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export LIBRARY_PATH=$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/ncurses
+ls $LD_LIBRARY_PATH
 sed -i "s/\s8000/ 1/" bam_plcmd.c
+sed -i 's/-lcurses/-lncurses/' Makefile
 make
 mkdir -p $PREFIX/bin
 mv samtools $PREFIX/bin
